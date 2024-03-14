559b71caf73ef0cfffbcd3f32c164a2b95d04c24
200Pranav <132340400+200Pranav@users.noreply.github.com>
2024-03-14 17:31:36 +0530

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+//create Schema
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    age: {
+        type: Number,
+        required: true,
+
+    }
+},
+{timestamps:true}
+);
+
+//create Model
+
+const User = mongoose.model('User', userSchema)
+module.exports = User;
